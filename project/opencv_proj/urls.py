041efe8a53f1c8416e2e7305a4ce14544f9a27df
efaunt <iwata.mich@gmail.com>
2021-03-27 21:04:53 -0700

    views.1

--- project/opencv_proj/urls.py
+++ project/opencv_proj/urls.py
@@ -18,6 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import settings
 from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
